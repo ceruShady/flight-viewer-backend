eb6edc6c9cb3857d5df35f4d46df7db9f9ce6887
Tok Chun Lim <qiop1397@gmail.com>
2025-05-19 13:54:13 +0800

    Cleanup controllers/geopoints.js

--- controllers/geopoints.js
+++ controllers/geopoints.js
@@ -1,21 +1,20 @@
 const axios = require("axios");
 
 const API_KEY = process.env.API_KEY;
+const API_URL = process.env.API_URL;
 
 exports.getFixes = async (req, res) => {
-  const response = await axios.get(
-    "https://api.swimapisg.info/geopoints/list/fixes",
-    { headers: { apikey: API_KEY } }
-  );
+  const response = await axios.get(`${API_URL}/geopoints/list/fixes`, {
+    headers: { apikey: API_KEY },
+  });
 
   res.json(response.data);
 };
 
 exports.getAirways = async (req, res) => {
-  const response = await axios.get(
-    "https://api.swimapisg.info/geopoints/list/airways",
-    { headers: { apikey: API_KEY } }
-  );
+  const response = await axios.get(`${API_URL}/geopoints/list/airways`, {
+    headers: { apikey: API_KEY },
+  });
 
   res.json(response.data);
 };
